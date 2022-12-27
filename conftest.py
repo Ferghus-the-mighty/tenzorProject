58c0bee03607cab6b503d127779b5ba16e1dd4ce
Ferghus-the-mighty <52192850+Ferghus-the-mighty@users.noreply.github.com>
2022-12-28 01:08:16 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     yield driver
     driver.quit()
+    
